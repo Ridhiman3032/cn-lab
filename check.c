ec3e5dcd98b640268f63e57573ef75f19f2b5f6a
Ridhiman3032 <114087406+Ridhiman3032@users.noreply.github.com>
2022-12-13 00:33:29 +0530

    Add files via upload

--- check.c
+++ check.c
@@ -1,1 +1,54 @@
+#include<stdio.h>
+#include<math.h>
+int sender(int arr[10],int n)
+{
+    int checksum,sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+    }
+    printf("SUM IS: %d",sum);
+    checksum=~sum;
+    printf("\nChecksum at sender is %d",checksum);
+    return checksum;
+}
 
+int receiver(int arr[10],int n,int sch)
+{
+    int checksum,sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+    }
+    printf("SUM IS:%d",sum);
+    sum=sum+sch;
+    checksum=~sum;
+    printf("\nChecksum at receiver is %d",checksum);
+    return checksum;
+}
+int main()
+{
+    int sch,rch,n;
+    printf("\nEnter number of bytes: ");
+    scanf("%d",&n);
+    int arr[n],rec[n];
+    printf("\nEnter the data byte wise:\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    sch=sender(arr,n);
+
+    printf("\nEnter the data received byte wise:\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&rec[i]);
+    }
+    rch=receiver(rec,n,sch);
+
+    if(rch!=0)
+        printf("\nErrors");
+    else
+        printf("\nno Errors");
+}
